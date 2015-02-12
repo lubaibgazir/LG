2ffb6449e9607884b61a8a2d70e350507c30fbcc
Lubaib Gazir <lubaib.gazir@sourcebits.com>
2015-02-12 08:29:37 +0530

    angular app generated

---
+++ app/js/app.js
@@ -0,0 +1,44 @@
+
+/*================================================================
+=>                  App = lg
+==================================================================*/
+/*global angular*/
+
+var app = angular.module('lg', ["ngCookies", "ngResource", "ngSanitize", "ngRoute", "ngAnimate", "ui.utils", "ui.router"]);
+
+
+app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+	'use strict';
+
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'templates/home.html'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		});
+
+	$locationProvider.hashPrefix('!');
+
+	// This is required for Browser Sync to work poperly
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+}]);
+
+
+/*================================================================
+=>                  lg App Run()  
+==================================================================*/
+
+app.run(['$rootScope', function ($rootScope) {
+	
+	'use strict';
+
+	console.log('Angular.js run() function...');
+}]);
+
+
+
+
+/* ---> Do not delete this comment (Values) <--- */
+
+/* ---> Do not delete this comment (Constants) <--- */
