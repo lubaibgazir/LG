a85051d4631aa91a1127af2047c3ddf95052ea6b
Lubaib Gazir <lubaib.gazir@sourcebits.com>
2015-02-20 08:26:50 +0530

    routing works

--- app/js/app.js
+++ app/js/app.js
@@ -7,21 +7,22 @@
 var app = angular.module('lg', ["ngCookies", "ngResource", "ngSanitize", "ngRoute", "ngAnimate", "ui.utils", "ui.router"]);
 
 
-app.config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+app.config(['$routeProvider', '$locationProvider', '$httpProvider', '$stateProvider', '$urlRouterProvider', function ($routeProvider, $locationProvider, $httpProvider, $stateProvider, $urlRouterProvider) {
 	'use strict';
 
-	$routeProvider
-		.when('/', {
-			url: '/',
-			templateUrl: 'templates/layout.html'
+	$stateProvider
+		.state('layout', {
+			url: '/layout',
+			templateUrl: 'templates/layout.html',
 		})
-		.when('/welcome', {
+		.state('layout.welcome', {
 			url: '/welcome',
 			templateUrl: 'templates/welcome.html'
-		})
-		.otherwise({
-			redirectTo: '/'
 		});
+
+		
+
+		$urlRouterProvider.otherwise('layout/welcome');
 
 	//$locationProvider.hashPrefix('!');
 
