19277bcaff87261a03b5f08ee0a235e531de895b
Lubaib Gazir <lubaib.gazir@sourcebits.com>
2015-07-09 23:07:18 +0530

    skills controller

---
+++ app/js/controllers/skills.js
@@ -0,0 +1,46 @@
+
+/*================================================================
+=>                  Controller = Skills
+==================================================================*/
+/*global app*/
+
+app.controller('SkillsCtrl', ['$scope', function ($scope) {
+
+	'use strict';
+
+	$scope.skillsItems = [
+
+		{
+			"title" : "HTML5/CSS3",
+			"imgSrc" : "img/skills/htmlcss.jpg",
+			"strength" : "99%"
+		},
+		{
+			"title" : "AngularJS",
+			"imgSrc" : "img/skills/angularjs.jpg",
+			"strength" : "75%"
+		},
+		{
+			"title" : "Javascript",
+			"imgSrc" : "img/skills/js.jpg",
+			"strength" : "60%"
+		},
+		{
+			"title" : "Responsive Web",
+			"imgSrc" : "img/skills/responsive.jpg",
+			"strength" : "99%"
+		},
+		{
+			"title" : "Photoshop",
+			"imgSrc" : "img/skills/photoshop.jpg",
+			"strength" : "95%"
+		}
+	];
+	
+}]);
+
+
+/*-----  End of Controller = Skills  ------*/
+
+
+
